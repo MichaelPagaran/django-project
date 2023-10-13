4cff8cb571e51df4059d2a89d0542466e43b7456
michaelp <michaelp@cytechint.com>
2023-10-13 23:55:14 +0800

    add github app

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
 
+router = routers.DefaultRouter()
+
+router.register(r'')
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
